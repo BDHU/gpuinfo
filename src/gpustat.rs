f6e1b446e8b182a7ad6d34774021da03e60c0e1e
BDHU <bodunhu@utexas.edu>
2021-06-22 05:20:50 +0000

    add MiG device handle

--- src/gpustat.rs
+++ src/gpustat.rs
@@ -1,4 +1,5 @@
 use sysinfo::{ProcessExt, System, SystemExt};
+use std::os::raw;
 
 use nvml_wrapper::enum_wrappers::device::{
     TemperatureSensor,
@@ -7,6 +8,11 @@ use nvml_wrapper::enums::device::UsedGpuMemory;
 use nvml_wrapper::error::NvmlError as NvmlError;
 use nvml_wrapper::structs::device::*;
 use nvml_wrapper::struct_wrappers::device::*;
+
+use nvml_wrapper_sys::bindings::{
+    nvmlDevice_t,
+    NvmlLib,
+};
 
 pub struct GPUstat {
     name: Result<String, NvmlError>,
@@ -34,6 +40,7 @@ pub fn read_gpu_stat(device: &nvml_wrapper::Device) -> GPUstat {
     return gpustat;
 }
 
+#[cfg(not(target_os = "linux"))]
 pub fn dump_gpu_stat(device: nvml_wrapper::Device) {
     let gpustat = read_gpu_stat(&device);
     let mut result = "".to_owned();
@@ -102,6 +109,95 @@ pub fn dump_gpu_stat(device: nvml_wrapper::Device) {
         result.push_str("No running processes found");
     } else {
         result.push_str(&graphics_processes);
+    }
+
+    println!("{}", result);
+}
+
+#[cfg(target_os = "linux")]
+pub fn dump_gpu_stat(device: nvml_wrapper::device::Device) {
+    let gpustat = read_gpu_stat(&device);
+    let mut result = "".to_owned();
+
+    let id = match gpustat.id {
+        Ok(id) => format!("[{}]: ", id),
+        Err(_err) => "".to_string(),
+    };
+    result.push_str(&id);
+    
+    let name = match gpustat.name {
+        Ok(name) => format!("{} | ", name),
+        Err(_err) => "".to_string(),
+    };
+    result.push_str(&name);
+
+    let compute_capability = match gpustat.compute_capability {
+        Ok(compute_capability) => format!("{}{} | ", compute_capability.major, compute_capability.minor),
+        Err(_err) => "".to_string(),
+    };
+    result.push_str(&compute_capability);
+
+    let utilization_rates = match gpustat.utilization_rates {
+        Ok(utilization_rates) => format!("{:>3} % | ", utilization_rates.gpu),
+        Err(_err) => "".to_string(),
+    };
+    result.push_str(&utilization_rates);
+
+    let memory_info = match gpustat.memory_info {
+        Ok(memory_info) => format!("{:>width$} / {:<width$} MB | ", memory_info.used / 1024 / 1024, memory_info.total / 1024 / 1024,
+            width = (memory_info.total / 1024 / 1024).to_string().chars().count()),
+        Err(_err) => "".to_string(),
+    };
+    result.push_str(&memory_info);
+
+    // let fan_speed = match gpustat.fan_speed {
+    //     Ok(fan_speed) => format!("{:>3} % | ", fan_speed),
+    //     Err(_err) => "".to_string(),
+    // };
+    // result.push_str(&fan_speed);
+
+    let temperature = match gpustat.temperature {
+        Ok(temperature) => format!("{:>3}°C | ", temperature),
+        Err(_err) => "".to_string(),
+    };
+    result.push_str(&temperature);
+
+    let sys: sysinfo::System = System::new_all();
+
+    let graphics_processes = match gpustat.running_graphics_processes {
+        Ok(processes) => {
+            let mut all_processes_info = String::new();
+            for p in processes.iter() {
+                let p_name = get_process_name(&sys, p.pid);
+                let used_mem = match p.used_gpu_memory {
+                    UsedGpuMemory::Used(used) => used,
+                    _ => 0,
+                };
+                all_processes_info.push_str(&format!("{}: {}MB  ", p_name, used_mem / 1024 / 1024));
+            }
+            all_processes_info
+        },
+        Err(_err) => "".to_string(),
+    };
+    if graphics_processes.chars().count() < 1 {
+        result.push_str("No running processes found");
+    } else {
+        result.push_str(&graphics_processes);
+    }
+
+    unsafe {
+        let mut is_mig_device: raw::c_uint = 0 as raw::c_uint;
+        let is_mig_device_ptr: *mut raw::c_uint = &mut is_mig_device as *mut raw::c_uint;
+        let raw_device_handle: nvmlDevice_t = device.handle();
+        let nvml_lib = NvmlLib::new("libnvidia-ml.so").unwrap();
+        let mut new_mig_device_handle: nvmlDevice_t = 0 as nvmlDevice_t;
+        let new_mig_device_handle_ptr: *mut nvmlDevice_t = &mut new_mig_device_handle as *mut nvmlDevice_t;
+        nvml_lib.nvmlDeviceGetMigDeviceHandleByIndex(raw_device_handle, 0 as raw::c_uint, new_mig_device_handle_ptr);
+
+        if nvml_lib.nvmlDeviceIsMigDeviceHandle(new_mig_device_handle, is_mig_device_ptr) != nvml_wrapper_sys::bindings::nvmlReturn_enum_NVML_SUCCESS {
+            println!("return error");
+        }
+        println!("Device is {}", is_mig_device);
     }
 
     println!("{}", result);
